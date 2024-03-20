bd81c66e5057ee353c648ad51242628320837f5c
Oksana Kolisnyk <zazera777@gmail.com>
2024-03-20 17:03:42 +0200

    +task1-5, +[], +for(), +while(), +do{} while(), +for(of){}

--- exercises/lecture-21/app.js
+++ exercises/lecture-21/app.js
@@ -1,26 +1,53 @@
+console.log("`NOTE`: String -> [], fruits ==> []");
+// THE TASK 1 SECTION
+let fruits = 'apple banana cantaloupe blueberries grapefruit';
+fruits = fruits.split(" ");
+console.log(fruits);
 // THE TASK 1 SECTION
 
-// THE TASK 1 SECTION
+console.log("\n`NOTE`: 'for(){}' loop shows each item of []");
 
 // THE TASK 2 SECTION
-
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
 // THE TASK 2 SECTION
 
-// THE TASK 3 SECTION
+console.log("\n`NOTE`: 'while(){}' loop shows each item of []");
 
 // THE TASK 3 SECTION
+let fruitElement = 0;
+while (fruitElement < fruits.length) {
+  console.log(fruits[fruitElement]);
+  fruitElement++;
+}
+// THE TASK 3 SECTION
+
+console.log("\n`NOTE`: 'do{} while()' loop shows each item of []");
 
 // THE TASK 4 SECTION
-
+let index = 0;
+do {
+  console.log(fruits[index]);
+  index++;
+} while (index < fruits.length);
 // THE TASK 4 SECTION
 
-// THE TASK 5 SECTION
+console.log("\n`NOTE`: 'for(of){}' loop shows each item of []");
 
 // THE TASK 5 SECTION
+for (const fruit of fruits) {
+  console.log(fruit);
+}
+// THE TASK 5 SECTION
+
+console.log("\n`NOTE`: 'do{} while()' loop shows each item of []");
 
 // THE TASK 6 SECTION
 
 // THE TASK 6 SECTION
+
+console.log("\n`NOTE`: 'do{} while()' loop shows each item of []");
 
 // THE TASK 7 SECTION
 
