8cee11e3297c848ad900b30f1d2b56a115992278
Oksana Kolisnyk <zazera777@gmail.com>
2024-03-25 18:55:29 +0200

    Added the main files, doctype in index.html, description in README, task1 in app.js

---
+++ exercises/lecture-23/app.js
@@ -0,0 +1,46 @@
+// THE TASK 1 SECTION
+const alert = document.querySelector(".alert");
+//++
+function clearAlertClasses() {
+  while (alertOne.classList.length > 0) {
+    alertOne.classList.remove(alertOne.classList.item(0));
+  }
+  alertOne.classList.add("alert", "mt-5");
+  alertOne.textContent = "";
+}
+
+let showAlertOne = function () {
+  clearAlertClasses();
+  alertOne.classList.add("alert-primary");
+  alertOne.textContent = "A simple primary alert—check it out!";
+};
+
+// THE TASK 1 SECTION
+
+// THE TASK 2 SECTION
+
+// THE TASK 2 SECTION
+
+// THE TASK 3 SECTION
+
+// THE TASK 3 SECTION
+
+// THE TASK 4 SECTION
+
+// THE TASK 4 SECTION
+
+// THE TASK 5 SECTION
+
+// THE TASK 5 SECTION
+
+// THE TASK 6 SECTION
+
+// THE TASK 6 SECTION
+
+// THE TASK 7 SECTION
+
+// THE TASK 7 SECTION
+
+// THE TASK 8 SECTION
+
+// THE TASK 8 SECTION
