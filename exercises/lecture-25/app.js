e76a9ad4163f92b892ce35f792b9f0eb3970fa93
Oksana Kolisnyk <zazera777@gmail.com>
2024-03-29 19:11:17 +0200

    Added task2-3, createElement(), appendChild()

--- exercises/lecture-25/app.js
+++ exercises/lecture-25/app.js
@@ -4,7 +4,7 @@ const secondTask = document.getElementById("secondTask");
 const thirdTask = document.getElementById("thirdTask");
 
 // THE TASK 1 SECTION
-const list = ['html', 'css', 'javascript', 'react.js'];
+const list = ["html", "css", "javascript", "react.js"];
 //++
 const ulList = document.createElement("ul");
 list.forEach((item) => {
@@ -18,15 +18,63 @@ firstTask.appendChild(ulList);
 
 // THE TASK 2 SECTION
 const listWithHref = [
-  {'html': "https://developer.mozilla.org/en-US/docs/Web/HTML"}, 
-  {'css': "https://developer.mozilla.org/en-US/docs/Web/CSS"}, 
-  {'javascript': "https://developer.mozilla.org/en-US/docs/Web/JavaScript"}, 
-  {'react.js': "https://react.dev"}
+  { html: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
+  { css: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
+  { javascript: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
+  { "react.js": "https://react.dev" },
 ];
 //++
+const orderedList = document.createElement("ol");
+listWithHref.forEach((obj) => {
+  const liItem = document.createElement("li");
+  const addLink = document.createElement("a");
+  addLink.href = Object.values(obj)[0];
+  addLink.textContent = Object.keys(obj)[0];
+  liItem.appendChild(addLink);
+  orderedList.appendChild(liItem);
+});
 
+secondTask.appendChild(orderedList);
 // THE TASK 2 SECTION
 
 // THE TASK 3 SECTION
+const students = [
+  { firstName: "Tom", lastName: "Cat", degree: 230 },
+  { firstName: "Nary", lastName: "Ann", degree: 336 },
+  { firstName: "John", lastName: "Doe", degree: 400 },
+  { firstName: "James", lastName: "Bond", degree: 700 },
+];
+//++
+function createTable() {
+  const table = document.createElement("table");
+  const thead = document.createElement("thead");
+  const headerRow = document.createElement("tr");
+  const headers = ["First Name", "Last Name", "Degree"];
+
+  headers.forEach((headerText) => {
+    const th = document.createElement("th");
+    th.textContent = headerText;
+    headerRow.appendChild(th);
+  });
+
+  thead.appendChild(headerRow);
+  table.appendChild(thead);
+  const tbody = document.createElement("tbody");
+
+  students.forEach((student) => {
+    const row = document.createElement("tr");
+    const { firstName, lastName, degree } = student;
+    row.innerHTML = `
+          <td>${firstName}</td>
+          <td>${lastName}</td>
+          <td>${degree}</td>
+      `;
+    tbody.appendChild(row);
+  });
+  table.appendChild(tbody);
+  thirdTask.appendChild(table);
+}
+
+createTable();
 
 // THE TASK 3 SECTION
