7d447b67e459059fa55998e4b1cdab14aa1315ec
Oksana Kolisnyk <zazera777@gmail.com>
2024-04-03 19:04:02 +0300

    Optimized code, added trim()

--- exercises/lecture-27/app.js
+++ exercises/lecture-27/app.js
@@ -1,3 +1,29 @@
 // THE TASK 1 SECTION
+const ul = document.querySelector("ul");
+const input = document.getElementById("item");
+let itemsArray = JSON.parse(localStorage.getItem("items")) || [];
 
+function addTask(text) {
+  const li = document.createElement("li");
+  li.textContent = text;
+  ul.appendChild(li);
+}
+
+itemsArray.forEach(addTask);
+
+function add() {
+  const taskText = input.value.trim();
+  if (taskText !== "") {
+    itemsArray.push(taskText);
+    localStorage.setItem("items", JSON.stringify(itemsArray));
+    addTask(taskText);
+    input.value = "";
+  }
+}
+
+function del() {
+  localStorage.removeItem("items");
+  itemsArray = [];
+  ul.innerHTML = "";
+}
 // THE TASK 1 SECTION
