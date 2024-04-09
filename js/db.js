9b3c3bb92f3307c49cd8c6ec87af00a511561f04
Oksana Kolisnyk <zazera777@gmail.com>
2024-04-09 07:24:05 +0300

    Added products in catalog.html, added cart html structure

--- js/db.js
+++ js/db.js
@@ -2,16 +2,16 @@ let products = [
   {
     id: 1,
     badge: {
-      title: "Sold",
-      bg: "sold",
+      title: "",
+      bg: "",
     },
-    image: "./assets/products/balancing-tone-laneige.jpg",
+    image: "../assets/products/balancing-tone-laneige.jpg",
     name: "Balancing tone of Laneige",
     price: 98,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
     stars: 3,
-    category: 1,
+    category: 4,
   },
   {
     id: 2,
@@ -19,13 +19,13 @@ let products = [
       title: "Sold",
       bg: "sold",
     },
-    image: "./assets/products/body-lotion-necessaire.jpg",
+    image: "../assets/products/body-lotion-necessaire.jpg",
     name: "Body lotion of Necessaire",
     price: 130,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
     stars: 5,
-    category: 2,
+    category: 7,
   },
   {
     id: 3,
@@ -33,13 +33,13 @@ let products = [
       title: "New",
       bg: "new",
     },
-    image: "./assets/products/body-oil-im-fabulous.jpg",
+    image: "../assets/products/body-oil-im-fabulous.jpg",
     name: "Body oil of I'm Fabulous",
     price: 105,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
     stars: 4,
-    category: 3,
+    category: 1,
   },
   {
     id: 4,
@@ -47,13 +47,13 @@ let products = [
       title: "",
       bg: "",
     },
-    image: "./assets/products/condition-mask-the-how-shop.jpg",
+    image: "../assets/products/condition-mask-the-how-shop.jpg",
     name: "Condition mask of The how shop",
     price: 91,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
     stars: 1,
-    category: 4,
+    category: 3,
   },
   {
     id: 5,
@@ -61,7 +61,7 @@ let products = [
       title: "Sale",
       bg: "sale",
     },
-    image: "./assets/products/hydrating-toner-botanical-republic.jpg",
+    image: "../assets/products/hydrating-toner-botanical-republic.jpg",
     name: "Hydrating toner of Botanical republic",
     price: 50,
     description:
@@ -76,7 +76,7 @@ let products = [
       title: "",
       bg: "",
     },
-    image: "./assets/products/perfume-chanel-gabrielle.jpg",
+    image: "../assets/products/perfume-chanel-gabrielle.jpg",
     name: "Perfume of Chanel Gabrielle",
     price: 159,
     description:
@@ -90,13 +90,13 @@ let products = [
       title: "Sale",
       bg: "sale",
     },
-    image: "./assets/products/perfume-chanel-number-5.jpg",
+    image: "../assets/products/perfume-chanel-number-5.jpg",
     name: "Perfume of Chanel Number 5",
     price: 169,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
     stars: 4,
-    category: 6,
+    category: 5,
   },
 
   {
@@ -105,13 +105,13 @@ let products = [
       title: "",
       bg: "",
     },
-    image: "./assets/products/perfume-ginger-white.jpg",
+    image: "../assets/products/perfume-ginger-white.jpg",
     name: "Perfume of Ginger White",
     price: 120,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
     stars: 3,
-    category: 7,
+    category: 5,
   },
 
   {
@@ -120,13 +120,13 @@ let products = [
       title: "Sold",
       bg: "sold",
     },
-    image: "./assets/products/perfume-leau-laurissi.jpg",
+    image: "../assets/products/perfume-leau-laurissi.jpg",
     name: "Perfume of L'eau Laurissi",
     price: 100,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
     stars: 5,
-    category: 7,
+    category: 5,
   },
 
   {
@@ -135,13 +135,13 @@ let products = [
       title: "New",
       bg: "new",
     },
-    image: "./assets/products/perfume-miss-dior.jpg",
+    image: "../assets/products/perfume-miss-dior.jpg",
     name: "Perfume of Miss Dior",
     price: 88,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
     stars: 5,
-    category: 7,
+    category: 5,
   },
   {
     id: 11,
@@ -149,13 +149,13 @@ let products = [
       title: "Sale",
       bg: "sale",
     },
-    image: "./assets/products/perfume-narciso-rodriguez.jpg",
+    image: "../assets/products/perfume-narciso-rodriguez.jpg",
     name: "Perfume of Narciso Rodriguez",
     price: 75,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
     stars: 0,
-    category: 7,
+    category: 5,
   },
 
   {
@@ -164,13 +164,13 @@ let products = [
       title: "New",
       bg: "new",
     },
-    image: "./assets/products/pet-cbd-oil-not-pot.jpg",
-    name: "Pet cbd oil of NOT POT",
+    image: "../assets/products/pet-cbd-oil-not-pot.jpg",
+    name: "Pet CBD oil of NOT POT",
     price: 200,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
     stars: 2,
-    category: 7,
+    category: 2,
   },
   {
     id: 13,
@@ -178,13 +178,13 @@ let products = [
       title: "Sale",
       bg: "sale",
     },
-    image: "./assets/products/tone-up-2ndesign.jpg",
+    image: "../assets/products/tone-up-2ndesign.jpg",
     name: "Tone up of 2ndesign",
     price: 210,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
     stars: 5,
-    category: 7,
+    category: 4,
   },
   {
     id: 14,
@@ -192,13 +192,13 @@ let products = [
       title: "Sale",
       bg: "sale",
     },
-    image: "./assets/products/treatment-cbd-nora.jpg",
-    name: "Treatment cbd of Nora",
+    image: "../assets/products/treatment-cbd-nora.jpg",
+    name: "Treatment CBD of Nora",
     price: 55,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
     stars: 4,
-    category: 7,
+    category: 2,
   },
   {
     id: 15,
@@ -206,58 +206,57 @@ let products = [
       title: "Sale",
       bg: "sale",
     },
-    image: "./assets/products/vitamin-sanitizer-marshol.jpg",
+    image: "../assets/products/vitamin-sanitizer-marshol.jpg",
     name: "Vitamin Sanitizer of Marshol",
     price: 77,
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut ullamcorper leo, eget euismod orci. Cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus. Vestibulum ultricies aliquam convallis.",
     stars: 5,
-    category: 7,
+    category: 6,
   },
 ];
 
 let categories = [
   {
     id: 1,
-    image: "./assets/category/body-oil-im-fabulous.jpg",
-    section: "Electronics",
-    name: "Headphone",
+    image: "../assets/category/body-oil-im-fabulous.jpg",
+    section: "Body & Face Oils",
+    name: "Body oils",
   },
   {
     id: 2,
-    name: "Keyboard",
-    image: "./assets/category/condition-mask-the-how-shop.jpg",
-    section: "Electronics",
+    image: "../assets/category/pet-cbd-oil-not-pot.jpg",
+    name: "Pet CBD oils",
+    section: "Body & Face Oils",
   },
   {
     id: 3,
-    image: "./assets/category/hydrating-toner-botanical-republic.jpg",
-    name: "T-Shirts",
-    section: "Fashion & Acc",
+    name: "Condition masks",
+    image: "../assets/category/condition-mask-the-how-shop.jpg",
+    section: "Hair & Deep Clean",
   },
   {
     id: 4,
-    image: "./assets/category/perfume-chanel-gabrielle.jpg",
-    name: "Watches",
-    section: "Fashion & Acc",
+    image: "../assets/category/hydrating-toner-botanical-republic.jpg",
+    name: "Toners",
+    section: "Body & Face Toners",
   },
   {
     id: 5,
-    image: "./assets/category/pet-cbd-oil-not-pot.jpg",
-    name: "Shoes",
-    section: "Fashion & Acc",
+    image: "../assets/category/perfume-chanel-gabrielle.jpg",
+    name: "Perfumes",
+    section: "Perfumes & Girls",
   },
-
   {
     id: 6,
-    image: "./assets/category/vitamin-sanitizer-marshol.jpg",
-    name: "Perfume",
-    section: "Health & Beauty",
+    image: "../assets/category/vitamin-sanitizer-marshol.jpg",
+    name: "Vitamin sanitizers",
+    section: "Body & Deep Clean",
   },
   {
     id: 7,
-    image: "./assets/category/shower-gel-because-its-you.jpg",
-    name: "Smartwatch",
-    section: "Health & Beauty",
+    image: "../assets/category/shower-gel-because-its-you.jpg",
+    name: "Shower gels",
+    section: "Body & Deep Clean",
   },
 ];
